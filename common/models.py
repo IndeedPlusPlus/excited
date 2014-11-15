faed4046e9f8efae9596a01b28ba204b16cae49a
Indeed <indeed@indeedblog.net>
2014-11-15 21:18:45 +0800

    fix primary key problem>

--- common/models.py
+++ common/models.py
@@ -3,14 +3,12 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
     email = models.EmailField(max_length=255)
     nickname = models.CharField(max_length=255)
     password_hash = models.CharField(max_length=255)
 
 
 class Item(models.Model):
-    id = models.BigIntegerField(primary_key=True)
     title = models.TextField(max_length=65536)
     content = models.TextField(max_length=65536)
     created_on = models.BigIntegerField(default=get_time_milliseconds, blank=True)
