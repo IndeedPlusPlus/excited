9401d2106193e323e383eec41cba981bca3d48b5
Indeed <indeed@indeedblog.net>
2014-11-15 16:46:02 +0800

    tweak models.

--- common/models.py
+++ common/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    password_hash = models.CharField(max_length=255)
+
+
+class Item(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    owner = models.IntegerField(db_index=True)
+    title = models.TextField(max_length=65576)
+    content = models.TextField(max_length=65576)
+
+
