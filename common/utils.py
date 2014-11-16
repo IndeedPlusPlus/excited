6d210f4c4bcd4c867bd194a760458bf8b8269243
Indeed <indeed@indeedblog.net>
2014-11-16 09:03:14 +0800

    fix timestamp not updated.

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 
-def get_time_milliseconds(time=datetime.now()):
+def get_time_milliseconds(time=None):
+    if time is None:
+        time = datetime.now()
     delta = time - datetime(1970, 1, 1)
     return int(round(delta.total_seconds() * 1000))
